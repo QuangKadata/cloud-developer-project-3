32bbdd095f6508f95c1c35227c17f3073c778d21
nhnam <37695601+hoai97nam@users.noreply.github.com>
2023-12-07 16:40:50 +0700

    commit project 3 quangdv8

---
+++ udagram-api-user/src/sequelize.ts
@@ -0,0 +1,19 @@
+import {Sequelize} from 'sequelize-typescript';
+import {config} from './config/config';
+
+
+export const sequelize = new Sequelize({
+  database: config.database,
+  username: config.username,
+  password: config.password,
+  host: config.host,
+  port: 5432,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      // navtive: true,
+      // require: false,
+      rejectUnauthorized: false
+    }
+  },
+});
